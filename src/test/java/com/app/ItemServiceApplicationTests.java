46b34a734823f92de6df38f8b952df2fa997b443
Bruno C. Manso <mrbrunomanso@gmail.com>
2023-03-26 10:56:03 -0300

    working implementation

--- src/test/java/com/app/ItemServiceApplicationTests.java
+++ src/test/java/com/app/ItemServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bighiccups.qroopenapi;
+package com.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QroOpenApiApplicationTests {
+class ItemServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
