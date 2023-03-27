410c3d8605ffa1ec3e894e558c39f22ccd20de8e
Bruno C. Manso <mrbrunomanso@gmail.com>
2023-03-27 17:06:25 -0300

    cors config | endpoint adjustment

--- src/main/java/com/app/controller/BotController.java
+++ src/main/java/com/app/controller/BotController.java
@@ -4,11 +4,14 @@ import com.app.model.request.BotRequest;
 import com.app.model.response.ChatGptResponse;
 import com.app.service.BotService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/bot")
 @RequiredArgsConstructor
